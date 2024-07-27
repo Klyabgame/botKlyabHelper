e340a59a203e6ca3576bda5dc2db1402eab8170c
unknown <gavino_10@hotmail.es>
2024-07-26 22:14:57 -0500

    first commit

---
+++ src/app.ts
@@ -0,0 +1,28 @@
+import express from 'express';
+import { envs } from './config';
+import { OpenAIController} from './presentation/openAI/controller';
+import cors from 'cors';
+
+(()=>{
+
+  main();
+  
+})();
+
+function main(){
+
+    const app=express();
+    app.use( express.json() ); // raw
+    app.use( express.urlencoded({ extended: true }) ); // x-www-form-urlencoded
+    app.use(cors());
+
+    const controller=new OpenAIController();
+
+    app.post('/api/openAI',controller.postMessageGPT)
+
+    
+
+    app.listen(envs.PORT,()=>{
+      console.log(`APP running on port ${envs.PORT}`);
+    })
+}
---
+++ src/config/envs.ts
@@ -0,0 +1,11 @@
+import 'dotenv/config';
+import { get } from 'env-var';
+
+
+export const envs = {
+
+  PORT: get('PORT').required().asPortNumber(),
+  CLAVE_API_OPENAI: get('CLAVE_API_OPENAI').default('public').asString(),
+  DISCORD_TOKEN: get('DISCORD_TOKEN').default('public').asString()
+
+}
