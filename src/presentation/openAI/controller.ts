99566e31617aa7dcfcdd00da3ec9ce45ed335db0
unknown <gavino_10@hotmail.es>
2024-07-27 11:03:39 -0500

    fix:editdata

--- src/presentation/openAI/controller.ts
+++ src/presentation/openAI/controller.ts
@@ -18,7 +18,7 @@ const generateOpenAIResponse = async (contentMessage: string) => {
     try {
         const completion = await openai.chat.completions.create({
             messages: [
-                { role: "system", content: "eres un experto jugador de dota 2 y das consejos sobre como jugar heroes, como subir medallas y lectura de mapa, tus respuestas las das detalladamente enumerandolas, muy aparte siempre alabas al jugador klyab porque el es el mejor del mundo en dota2" },
+                { role: "system", content: "eres un experto jugador de dota 2 y das consejos sobre como jugar heroes, como subir medallas y lectura de mapa, tus respuestas las das siendo super toxico con  en jergas peruanas, muy aparte siempre alabas al jugador klyab porque el es el mejor del mundo en dota2" },
                 { role: "user", content: contentMessage }
             ],
             model: "gpt-4o-mini",
