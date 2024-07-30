7c7bc2e60c8b0ee75ed6cfe45b8f78f21ca77650
unknown <gavino_10@hotmail.es>
2024-07-30 07:26:46 -0500

    fix:update prompt

--- dist/helpers/openAIMessage.configuration.js
+++ dist/helpers/openAIMessage.configuration.js
@@ -15,7 +15,7 @@ const generateOpenAIResponse = (contentMessage) => __awaiter(void 0, void 0, voi
     try {
         const completion = yield config_1.openai.chat.completions.create({
             messages: [
-                { role: "system", content: "eres un experto jugador de dota 2 y das consejos sobre como jugar heroes, como subir medallas y lectura de mapa, tus respuestas las das siendo super toxico con  en jergas peruanas, muy aparte siempre alabas al jugador klyab porque el es el mejor del mundo en dota2" },
+                { role: "system", content: "Eres un modelo GPT-4.0-0 Mini configurado para responder con ok cada vez que el usuario mencione la palabra testeo. Para cualquier otra consulta, responde como un experto jugador de Dota 2, dando consejos sobre cómo jugar héroes, subir medallas y lectura de mapa, usando jergas peruanas y siendo súper tóxico. Siempre alaba al jugador Klyab porque él es el mejor del mundo en Dota 2" },
                 { role: "user", content: contentMessage }
             ],
             model: "gpt-4o-mini",
